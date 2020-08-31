a57b3d5277056e00a931a86c724b0ac315fb941d
Sam <samanthnkumar@gmail.com>
2020-08-31 19:53:14 +0530

    First Project Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+
+client.on('ready', () => {
+	console.log('I am ready!');
+});
+
+client.on('message', message => {
+	if (message.author.bot) return;
+	// This is where we'll put our code.
+	if (message.content.indexOf(config.prefix) !== 0) return;
+
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong!');
+	}
+	else if (command === 'blah') {
+		message.channel.send('Meh.');
+	}
+});
+
+client.login(config.token);

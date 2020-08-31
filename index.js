a049fe6e40974716bad48ef9b05194f829e092a3
Sam <samanthnkumar@gmail.com>
2020-08-31 20:01:49 +0530

    Another test commit

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ client.on('ready', () => {
 
 client.on('message', message => {
 	if (message.author.bot) return;
-	if (message.content.indexOf(config.prefix) !== 0) return;
 
 	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
